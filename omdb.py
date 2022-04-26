6e0fdcc8973fe53dd961b996f04a97dc9ad13bc2
Apoorva Asthana <37489185+asthanaa16@users.noreply.github.com>
2022-04-26 13:38:53 -0400

    Update omdb.py

--- omdb.py
+++ omdb.py
@@ -1,18 +1,12 @@
 import json, requests, copy
-from final_proj_tree import TreeNode as Tree
-from final_proj_tree import BinarySearchTree as BST
-
-BASE_URL = 'http://www.omdbapi.com/?apikey=4697b61d&'
 
 
-
-def get_omdb_resource(url, params=None, timeout=10):
-    if params:
-        return requests.get(url, params, timeout=timeout).json()
-    else:
-        return requests.get(url, timeout=timeout).json()
+BASE_URL = 'http://www.omdbapi.com/?apikey=5e0924db&'
 
 
 genre_list = ['Action', 'Animation', 'Biopic','Comedy', 'Crime','Drama','Documentary','Fantasy','Historical','Horror','Romance','Science Fiction','Thriller','Musical','War']
+
+
+#print(requests.get(BASE_URL+'t=Shrek').json())
 
 
