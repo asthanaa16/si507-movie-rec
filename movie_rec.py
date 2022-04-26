7e354fbd08548dabecad3ea73f2c720cb2cd2057
Apoorva Asthana <37489185+asthanaa16@users.noreply.github.com>
2022-04-26 13:40:02 -0400

    Add files via upload

---
+++ movie_rec.py
@@ -0,0 +1,15 @@
+from matplotlib.font_manager import json_load
+from oscars_db import movies
+from omdb import BASE_URL
+import json, requests, copy
+from final_proj_tree import BinarySearchTree,TreeNode
+
+
+#STRUCTURE: requests.get(BASE_URL+'t='+movies[0][0]+'&y='+movie[0][1])
+json_movies = []
+for movie in movies:
+    movie_info = requests.get(BASE_URL + 't=' + movie[0] + '&y=' + movie[1]).json()
+    json_movies.append(movie_info)
+
+print(json_movies[0])
+
