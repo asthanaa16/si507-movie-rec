7ab609f5547486e6e23445a208e5bdaef2dc836e
Apoorva Asthana <37489185+asthanaa16@users.noreply.github.com>
2022-04-26 13:48:54 -0400

    Update final_proj_tree.py

--- final_proj_tree.py
+++ final_proj_tree.py
@@ -43,58 +43,37 @@ class TreeNode:
 
 
 
-class BinarySearchTree:
+class BinaryTree:
+    def __init__(self,rootObj):
+        self.key = rootObj
+        self.leftChild = None
+        self.rightChild = None
 
-    def __init__(self):
-        self.root = None
-        self.size = 0
-
-    def length(self):
-        return self.size
-
-    def __len__(self):
-        return self.size
-
-    def __iter__(self):
-        return self.root.__iter__()
-    def put(self,key,val):
-      if self.root:
-        self._put(key,val, self.root)
-      else:
-        self.root = TreeNode(key,val)
-      self.size = self.size + 1
-
-    def _put(self,key,val,currentNode):
-      if key < currentNode.key:
-        if currentNode.hasLeftChild():
-          self._put(key,val,currentNode.leftChild)
+    def insertLeft(self,newNode):
+        if self.leftChild == None:
+            self.leftChild = BinaryTree(newNode)
         else:
-          currentNode.leftChild = TreeNode(key,val,parent=currentNode)
-      else:
-        if currentNode.hasRightChild():
-            self._put(key,val,currentNode.rightChild)
+            t = BinaryTree(newNode)
+            t.leftChild = self.leftChild
+            self.leftChild = t
+
+    def insertRight(self,newNode):
+        if self.rightChild == None:
+            self.rightChild = BinaryTree(newNode)
         else:
-          currentNode.rightChild = TreeNode(key,val,parent=currentNode)
+            t = BinaryTree(newNode)
+            t.rightChild = self.rightChild
+            self.rightChild = t
 
-    def get(self,key):
-      if self.root:
-        res = self._get(key,self.root)
-        if res:
-          return res.payload
-        else:
-          return None
-      else:
-        return None
 
-    def _get(self,key,currentNode):
-      if not currentNode:
-        return None
-      elif currentNode.key == key:
-        return currentNode
-      elif key < currentNode.key:
-        return self._get(key,currentNode.leftChild)
-      else:
-        return self._get(key,currentNode.rightChild)
+    def getRightChild(self):
+        return self.rightChild
 
-    def __getitem__(self,key):
-        return self.get(key)
+    def getLeftChild(self):
+        return self.leftChild
+
+    def setRootVal(self,obj):
+        self.key = obj
+
+    def getRootVal(self):
+        return self.key
